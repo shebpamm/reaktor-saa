b3c8341f0de346c1547e0a145c55c0b502f3ea3d
Erik Karsten <shebpamm123@gmail.com>
2018-01-11 14:52:17 +0200

    Website ui almost done

--- client/js/menu_animations.js
+++ client/js/menu_animations.js
@@ -1,20 +1,51 @@
-function onEnter(e) {
-  $(e.target).addClass("active")
-  $(e.target).animate({height : 300}, 600, function() {
-    $(".test").show()
+var menuHeight;
+var numofentries = 9;
+
+function onClick(e) {
+  if($(e.target).closest('.content').length) return; //Check that menu is not triggered from content box
+  var elem = $(e.target).hasClass('list-row') ? $(e.target) : $(e.target).closest('.list-row'); //Check if element clicked was list-row or a child of it. Assign list-row to elem
+  if (elem.hasClass('active')) Close(elem);
+  else Open(elem);
+}
+
+function Open(elem) {
+
+  var headerHeight = elem.find('.location').height()
+
+  elem.addClass('active')
+  elem.find('.temperature').addClass("active-temp")
+  elem.find('.content').show(600);
+  elem.animate({height : 300}, 600, function() {
   })
 }
 
-function onLeave(e) {
-  $(e.target).removeClass("active")
-  $(e.target).animate({height : auto}, 600, function() {
-    $(".test").show()
-  })
+function Close(elem) {
+  elem.removeClass('active')
+  elem.find('.temperature').removeClass("active-temp")
+  elem.find('.content').hide(300);
+  elem.animate({height: menuHeight}, 600, function(){
+  });
+  elem.css('height', 'auto')
 }
 
+function resizeInput(e) {
+  $(e.target).animate({width : 25}, 600)
+  $(e.target).attr('placeholder', '');
+}
+
+function resizeInputDown(e) {
+  $(e.target).animate({width : 100}, 600, function() {
+    $(e.target).attr('placeholder', 'Uusi Lämpötila...');
+  })
+
+}
 
 $( document ).ready(function() {
     menus = $('.list-row');
-    menus.hover(onEnter, onLeave);
+    menuHeight = menus.first().height();
+    menus.click(onClick);
+    $("#tempInput").focus(resizeInput)
+    $("#tempInput").blur(resizeInputDown)
+
   //  menus.bind('oanimationend animationend webkitAnimationEnd', animationDone(e))
 });
